9b289df33b3283ebb22e89e9a4edea4e749f9f7e
nx2803 <nx2803@gmail.com>
2025-09-05 17:56:12 +0900

    v7

---
+++ 04/04.js
@@ -0,0 +1,65 @@
+//회문
+const check1 = (e) => {
+    //0 버튼의 submit 기능을 없에기
+    e.preventDefault();
+
+    //1. 사용자가 입력한 내용 가져오기
+    const inputV = document.getElementById('txt1').value;
+
+    //2. 사용자가 입력한 문자열을 뒤집기
+    //console.log(typeof (inputV));
+    //console.log(inputV.length);
+    //console.log(inputV[0]);
+
+
+    //입력 문자열 공백 제거
+    let inputVt = inputV.replaceAll(' ', '');
+    let resultV = '';
+    // for (let i = inputVt.length - 1; i >= 0; i--) {
+    //     console.log(inputVt[i]);
+    //     resultV += inputVt[i];
+    // }
+    resultV = inputVt.split('').reverse().join('');
+    console.log(resultV);
+
+
+    //3. 사용자가 입력한 문자열과 뒤집은 문자열이 같은지 비교
+    //3-1. 결과를 input 요소에 출력.
+    document.getElementById('txt2').value = inputV;
+    if (inputVt == resultV)
+        document.getElementById('txt2').value = `"${inputV}"는 회문입니다.`;
+    else
+        document.getElementById('txt2').value = `"${inputV}"는 회문이 아닙니다.`;
+    //3-2. 결과를 div 요소에 출력
+    document.getElementById('msg').innerHTML = `<span>"${inputV}"</span>는 ${inputVt == resultV ? "회문입니다." : "회문이 아닙니다."}`;
+}
+
+//숫자합계
+const check2 = () => {
+    //1 사용자가 입력한 문자열 가져오기
+    const inputV = document.getElementById('txt1').value;
+    //2 문자열에서 숫자인식 해서 더하기
+    let sum = 0;
+    // for (let i = 0; i < inputV.length; i++) {
+    //     if (!isNaN(inputV[i])) {
+    //         sum += parseInt(inputV[i]);
+    //     }
+    // }
+    for (let c of inputV) {
+        if (!isNaN(c)) {
+            sum += parseInt(c);
+        }
+    }
+    //3 합계 출력
+    document.getElementById('txt2').value = sum;
+    document.getElementById('msg').innerHTML = `합계 = &nbsp; <span>${sum}</span>`
+}
+
+//취소처리
+const check3 = () => {
+
+    document.getElementById('txt2').value = '';
+    document.getElementById('txt1').value = '';
+    document.getElementById('msg').innerHTML = '';
+    document.getElementById('txt1').focus();
+}
---
+++ 05/05.js
@@ -0,0 +1,79 @@
+//리터럴 표기법
+let arr1 = [1, 2, 3];
+
+
+//Array 생성자 이용
+let arr2 = new Array(1, 2, 3);
+
+//배열의 크기
+console.log(arr1)
+console.log(`arr1 : ${arr1}`)
+console.log(`arr1의 요소개수 : ${arr1.length}`)
+
+//배열요소 제거
+console.log(arr2)
+// arr2.length = 0;
+arr2 = [];
+console.log(arr2)
+
+// 배열요소 추가
+console.log(arr1)
+arr1.push('가')
+console.log(arr1)
+let arr1Pop = arr1.pop();
+console.log(arr1)
+console.log(arr1Pop)
+
+//배열 순회
+//1
+for (let i = 0; i < arr1.length; i++) {
+    console.log(arr1[i])
+}
+//2 key(index)값 가져오기
+for (let i in arr1) {
+    console.log(arr1[i])
+}
+
+//3 value 값 가져오기
+for (let item of arr1) {
+    console.log(item)
+}
+//4 forEach 메소드
+arr1.forEach((item, i) => {
+    console.log(i, item);
+});
+
+//배열 map 예습
+const arr3 = arr1.map((item) => {
+    return item * 2;
+})
+console.log(arr3)
+
+//콜백 함수는 파라미터가 1개이면 ()가 생략가능
+//return 문만 있으면 {}와 return을 생략 가능
+
+const arr4 = arr1.map(item => item * 2)
+console.log(arr4)
+
+//콜백 함수는 파라미터가 2개가 되면 인덱스를 가져옴
+const arr5 = arr1.map((item, i) => item * i);
+console.log(arr5)
+
+//filter
+const arr6 = arr1.filter(item => item % 2 == 0);
+console.log(arr6)
+
+//map함수 구현
+let arr7 = [];
+for (let c of arr1) {
+    arr7.push(c * 2);
+}
+console.log(arr7)
+//filter 함수
+let arr8 = [];
+for (let c of arr1) {
+    if (c % 2 == 0) {
+        arr8.push(c);
+    }
+}
+console.log(arr8)
